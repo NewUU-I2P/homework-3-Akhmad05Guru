f634d75bdf21c3621b06200dcdd0c5743cb8d804
Axmad <a.xasanov@newuu.uz>
2023-11-14 20:17:56 +0500

    1

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,6 +1,19 @@
 void problemSolution1(float consumed_water) {
     float cost;
-    // write your code here
+    if(consumed_water <= 30){
+        cost = consumed_water * 0.4 + 13
+    }
+    else if(consumed_water <= 50){
+        cost = (consumed_water - 30) * 0.12 + 30 * 0.4 + 13;
+    }
+    else if(consumed_water <= 60){
+        cost = (consumed_water - 50) * 1.4 +20 * 0.12 + 30 * 0.4 + 13;
+    }
+    else{
+        cost = (consumed_water - 60) * 1.5 + 10 * 1.4 + 20 * 0.12 + 30 * 0.4 + 13;
+    }
+
+
 
     return cost;
 }
--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,6 +2,32 @@
 
 std::string problemSolution3(float height, char S) {
     // write your code here
+    if (S == 'M'){
+        if(height < 1.70){
+            cout << S << "Short";
+        }
+        else if(height < 1.85){
+            cout <<S<< "Normal";
+        }
+        else{
+            cout <<S<< "Tall";
+        }
+    }
+    else{
+        if(height < 1.60){
+            cout << S << "Short";
+        }
+        else if(height < 1.75){
+            cout <<S<< "Normal";
+        }
+        else{
+            cout <<S<< "Tall";
+        }
+    }
+
+    return
+
+
 
     // use return to return your result
     // make use of control flow statements
